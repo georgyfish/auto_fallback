41561783a2f131d43a6f062d6fc4d25d144865d7
georgyfish <13056599771@163.com>
2024-07-01 18:15:12 +0800

    1# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch main# Your branch is up to date with 'origin/main'.## Changes to be committed:#	modified:   get_deb_version.py## Changes not staged for commit:#	modified:   __pycache__/get_commit.cpython-38.pyc#	modified:   __pycache__/get_deb_version.cpython-38.pyc#	modified:   __pycache__/sshClient.cpython-38.pyc#	modified:   get_commit.py#	modified:   log/log.txt#	modified:   sshClient.py#	modified:   test.py#	modified:   test.sh## Untracked files:#	log/log.txt.2024-06-28#	ssh.py#

--- get_deb_version.py
+++ get_deb_version.py
@@ -29,25 +29,58 @@ def get_deb_version(branch,begin_date,end_date):
     # error
     for work_date in work_date_list:
         url = f"https://oss.mthreads.com/product-release/{branch}/{work_date}/daily_build.txt"
-        log.logger.info(f"curl {url}")
-        try:
-            rs = subprocess.run(['curl', url], capture_output=True, text=True, check=True)
-            stdout_list = rs.stdout.splitlines()
-            log.logger.info(f"{stdout_list=}")
-            result.append(stdout_list)
-        except subprocess.CalledProcessError as e:
-        # 打印错误信息
-            log.logger.error(f"Error:\n{e.stderr}")
-        except Exception as e:
-        # 处理其他异常
-            log.logger.error(f"An unexpected error occurred: {e}")
+        if check_url(url):
+            log.logger.info(f"curl {url}")
+            try:
+                rs = subprocess.run(['curl',url], capture_output=True, text=True, check=True)
+                stdout_list = rs.stdout.splitlines()
+                log.logger.info(f"{stdout_list=}")
+                result.append(stdout_list)
+            except subprocess.CalledProcessError as e:
+            # 打印错误信息
+                log.logger.error(f"Error:\n{e.stderr}")
+            except Exception as e:
+            # 处理其他异常
+                log.logger.error(f"An unexpected error occurred: {e}")
+        else:
+            log.logger.error(f"The URL {url} is not accessible.")
     return result
 
 
+def check_url(url):
+    try:
+        # 使用 curl 命令检查 URL
+        # result = subprocess.run(
+        #     ['curl', '-Is', url], # 只获取头部信息来快速判断
+        #     stdout=subprocess.PIPE,
+        #     stderr=subprocess.PIPE
+        # )
+        result = subprocess.run(
+            ['wget', '--spider', '-q', url], # --spider 表示不下载，只检查，-q 表示安静模式
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        # 检查返回码是否为200
+        if result.returncode == 0:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
 
 if __name__ == '__main__':
-    driver_full_list = get_deb_version('develop','20240528', '20240601')
+    driver_full_list = get_deb_version('develop','20240624', '20240627')
     print(driver_full_list)
+    # 示例 URL
+    # url = "https://oss.mthreads.com/product-release/develop/20240625/daily_build.txt"
+    # 73ab64766
+    # url = "http://oss.mthreads.com/release-ci/gr-umd/develop/73ab64766_x86_64-mtgpu_linux-xorg-release-hw-glvnd.tar.gz"
+    # if check_url(url):
+    #     print(f"The URL {url} is valid and accessible.")
+    # else:
+    #     print(f"The URL {url} is not accessible.")
     # log = logManager('get_deb_version')
     # result = []
     # try:
