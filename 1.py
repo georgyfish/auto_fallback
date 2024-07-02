0cf066b127ed7c0c3d5fdb34d11b5649972cd72f
georgyfish <13056599771@163.com>
2024-07-03 01:22:17 +0800

    1# 请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交# 说明将会终止提交。## 位于分支 main# 您的分支与上游分支 'origin/main' 一致。## 要提交的变更：#	修改：     1.py## 尚未暂存以备提交的变更：#	修改：     log/log.txt#	修改：     sshClient.py## 未跟踪的文件:#	__pycache__/logManager.cpython-312.pyc#	log/log.txt.2024-06-29#

--- 1.py
+++ 1.py
@@ -6,7 +6,27 @@ import subprocess,os,sys,get_commit
 import sshClient
 from logManager import logManager
 import os,sys,time,re
+import test
 
+def test_dmesg(Pc,regexp):
+    rs = Pc.execute(f"dmesg |grep -iE 'mtgpu|mtsnd|MUSA|PVR'| \
+        grep -iE {regexp} |grep -v 'local error reg'")
+    Output = rs[0]
+    if not Output:
+        return True
+    else:
+        return False
+
+def test_perf(benchmark,Target_score):
+    rs = Pc.execute(f"/home/{exec_user}/xc_tool/testcase/xc_run_{benchmark}.sh ")
+    if not rs[0]:
+        score = rs[0].split('测试结果:')[-1]
+        if (Target_score - score)/Target_score*100 >= 5:
+            print(f"{benchmark}测试结果{score} less than {Target_score} 5%")
+            return False
+        else:
+            return True
+            
 branch = 'develop'
 begin_date = '20240620'
 end_date = '20240624'
@@ -16,6 +36,14 @@ Host_name = 'swqa'
 passwd = 'gfx123456'
 Pc = sshClient.sshClient(Test_Host_IP,Host_name,passwd)
 if 1000 == Pc.login():
-    # test_ssh()
-    rs = Pc.execute('uname -m')
+    rs = test.get_Pc_info(Pc)
     print(f"{rs=}")
+    glvnd, os_type, arch, architecture, dm_type, kernel_version, exec_user = (
+        rs['glvnd'], 
+        rs['os_type'], 
+        rs['arch'], 
+        rs['architecture'], 
+        rs['dm_type'], 
+        rs['kernel_version'], 
+        rs['exec_user']
+    )
